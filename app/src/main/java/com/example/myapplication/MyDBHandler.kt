973308d360584e88f5cc17000542562b47a4d1e9
Dwetzel <danjwetzel@gmail.com>
2021-02-24 23:53:22 -0800

    Revert "Merge branch 'master' of https://github.com/Dwetzel3/Shopping-Assistant"
    This reverts commit 9ca08990442caedeafa7e5cbda89e339dd5212a7, reversing
    changes made to e9a831aeebdd02de2c7e758196217888736fc59c.

--- app/src/main/java/com/example/myapplication/MyDBHandler.kt
+++ app/src/main/java/com/example/myapplication/MyDBHandler.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+
 class MyDBHandler(context: Context, name: String?,
                   factory: SQLiteDatabase.CursorFactory?, version: Int) : SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
@@ -14,6 +15,19 @@ class MyDBHandler(context: Context, name: String?,
 //                COLUMN_NAME
 //                + " TEXT," + ")")
 //        db.execSQL(CREATE_RECIPES_TABLE)
+    }
+
+    fun findSuggestion(suggestion: String): String? {
+        val sugg = "SELECT * FROM $TABLE_recipes WHERE $COLUMN_NAME LIKE  \"%$suggestion%\" ORDER BY RANDOM()"
+        val db = this.writableDatabase
+
+        db.rawQuery(sugg, null).use {
+            if (it.moveToFirst()) {
+                val result = it.getString(0)
+                return result
+            }
+        }
+        return null
     }
 
     fun findRecipe(recipeName: String): Recipe? {
@@ -45,26 +59,108 @@ class MyDBHandler(context: Context, name: String?,
 //        }
 
 
+        myTitles.clear()
+        myDetails.clear()
+        myImages.clear()
+        myInstructions.clear()
+        adapter!!.notifyDataSetChanged()
+
         while (cursor.moveToNext()) {
             val name = cursor.getString(0)
             val id = cursor.getInt(1)
             val ingredients = cursor.getString(8)
-            recipe = Recipe(id, name, ingredients)
+            val instructions = cursor.getString(9)
+            recipe = Recipe(id, name, ingredients, instructions)
 
-
-            myTitles.clear()
-            myDetails.clear()
-            myImages.clear()
-            adapter!!.notifyDataSetChanged()
 
             myTitles.add(name)
             myDetails.add(ingredients)
-            myImages.add(R.drawable.android)
+            myInstructions.add(instructions)
+            myImages.add(R.drawable.food)
             adapter!!.notifyDataSetChanged()
         }
 
+
+//        val c = cursor
+//        var buffer = StringBuffer()
+//        val name = cursor.getString(0)
+//        val id = cursor.getInt(1)
+//        val ingredients = cursor!!.getString(8)
+//        recipe = Recipe(id, name, ingredients)
+//
+//        while (cursor.moveToNext()) {
+//            buffer.append(" " + name)
+//        }
+
         return recipe
     }
+
+    fun findRecipeBrowse(recipeName: String): Recipe? {
+
+        val query =
+                "SELECT * FROM $TABLE_recipes WHERE $COLUMN_NAME LIKE  \"%$recipeName%\""
+
+        val idQuery =
+                "SELECT RecipeID FROM $TABLE_recipes WHERE $COLUMN_NAME =  \"$recipeName\""
+
+
+        val db = this.writableDatabase
+
+        val cursors = db.rawQuery(idQuery, null)
+
+        println("id is: " + cursors)
+
+        val cursor = db.rawQuery(query, null)
+
+        var recipe: Recipe? = null
+
+//        if (cursor.moveToFirst()) {
+//            cursor.moveToFirst()
+//
+//            val name = cursor.getString(0)
+//            val id = cursor.getInt(1)
+//            val ingredients = cursor.getString(8)
+//            recipe = Recipe(id, name, ingredients)
+//            cursor.close()
+//        }
+
+
+        myTitles.clear()
+        myDetails.clear()
+        myImages.clear()
+        myInstructions.clear()
+        browseAdapter!!.notifyDataSetChanged()
+
+        while (cursor.moveToNext()) {
+            val name = cursor.getString(0)
+            val id = cursor.getInt(1)
+            val ingredients = cursor.getString(8)
+            val instructions = cursor.getString(9)
+            recipe = Recipe(id, name, ingredients, instructions)
+
+
+            myTitles.add(name)
+            myDetails.add(ingredients)
+            myInstructions.add(instructions)
+            myImages.add(R.drawable.food)
+            browseAdapter!!.notifyDataSetChanged()
+        }
+
+
+//        val c = cursor
+//        var buffer = StringBuffer()
+//        val name = cursor.getString(0)
+//        val id = cursor.getInt(1)
+//        val ingredients = cursor!!.getString(8)
+//        recipe = Recipe(id, name, ingredients)
+//
+//        while (cursor.moveToNext()) {
+//            buffer.append(" " + name)
+//        }
+
+        return recipe
+    }
+
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int,
                            newVersion: Int) {
 
@@ -78,5 +174,7 @@ class MyDBHandler(context: Context, name: String?,
 
         val COLUMN_ID = "RecipeID"
         val COLUMN_NAME = "RecipeName"
+        val INGREDIENTS = "Ingredients"
     }
+
 }
--- app/src/main/java/com/example/myapplication/RecyclerAdapter.kt
+++ app/src/main/java/com/example/myapplication/RecyclerAdapter.kt
@@ -1,35 +1,38 @@
 package com.example.myapplication
 
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.Color.*
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
+import android.view.View.*
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import kotlinx.android.synthetic.main.list_layout.*
-import kotlinx.android.synthetic.main.list_layout.view.*
-import kotlinx.android.synthetic.main.list_layout.*
+import org.w3c.dom.Text
 
-internal lateinit var delBtn: Button
-internal lateinit var addBtn: Button
-
+internal lateinit var delBtn: ImageView
+internal lateinit var addBtn: ImageView
+internal lateinit var button: Button
 
 
 var myTitles = mutableListOf<String>()
-
+var ingredients = mutableListOf<String>()
 var myDetails = mutableListOf<String>()
+var myInstructions = mutableListOf<String>()
+var suggestions = mutableListOf<String>()
 
 var myImages = mutableListOf<Int>()
-var position = 0
+var position = RecyclerAdapter().getPosition()
 
 var layoutManager: RecyclerView.LayoutManager? = null
+var layoutManager2: RecyclerView.LayoutManager? = null
 var adapter: RecyclerView.Adapter<CustomViewHolder>? = null
+var browseAdapter: RecyclerView.Adapter<BrowseViewHolder>? = null
+var cartAdapter: RecyclerView.Adapter<CartViewHolder>? = null
+
 
 
 class RecyclerAdapter : RecyclerView.Adapter<CustomViewHolder>() {
@@ -37,9 +40,7 @@ class RecyclerAdapter : RecyclerView.Adapter<CustomViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val itemView = LayoutInflater.from(parent.context)
 
-
         val holder = CustomViewHolder(itemView.inflate(R.layout.list_layout, parent, false))
-
 
         return holder
     }
@@ -60,6 +61,7 @@ class RecyclerAdapter : RecyclerView.Adapter<CustomViewHolder>() {
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.itemTitle.text = myTitles[position]
         holder.itemDetail.text = myDetails[position]
+//        holder.itemInstructions.text = myInstructions[position]
         holder.itemImage.setImageResource(myImages[position])
     }
 }
@@ -68,16 +70,23 @@ class CustomViewHolder(val view: View): ViewHolder(view) {
     var itemImage: ImageView
     var itemTitle: TextView
     var itemDetail: TextView
+    var itemInstructions: TextView
 
 
     init {
         itemImage = itemView.findViewById(R.id.item_image)
         itemTitle = itemView.findViewById(R.id.item_title)
         itemDetail = itemView.findViewById(R.id.item_detail)
+        itemInstructions = itemView.findViewById(R.id.instructions)
+
 
         delBtn = itemView.findViewById(R.id.deleteButton)!!
         addBtn = itemView.findViewById(R.id.addButton)!!
 
+        // sets position on click
+        view.setOnClickListener {
+            RecyclerAdapter().setPosition(adapterPosition)
+        }
 
         //deletes item when x clicked
         delBtn!!.setOnClickListener {
@@ -90,7 +99,14 @@ class CustomViewHolder(val view: View): ViewHolder(view) {
         }
 
         view.setOnClickListener {
-            RecyclerAdapter().setPosition(adapterPosition)
+            println("Name: " + myTitles[adapterPosition])
+            if (itemInstructions.isVisible) {
+                itemInstructions.visibility = View.GONE
+            } else
+                itemInstructions.visibility = View.VISIBLE
+
+//            MainActivity().addToDetails(adapterPosition)
+        }
 
             //Changes color on click
 //            if (clicked) {
@@ -101,9 +117,119 @@ class CustomViewHolder(val view: View): ViewHolder(view) {
 //                view.setBackgroundColor(BLACK)
 //            }
 //            MainActivity().deleteItem(adapterPosition)
-            adapter?.notifyDataSetChanged()
+//            adapter?.notifyDataSetChanged()
 
+    }
+}
+
+class BrowseRecyclerAdapter : RecyclerView.Adapter<BrowseViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BrowseViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+
+        val holder = BrowseViewHolder(itemView.inflate(R.layout.list_layout, parent, false))
+
+        return holder
+    }
+
+    override fun getItemCount(): Int {
+        return myTitles.size
+
+    }
+    fun setPosition(newPosition:Int) {
+        position = newPosition
+    }
+
+    fun getPosition(): Int {
+        return position
+    }
+
+
+    override fun onBindViewHolder(holder: BrowseViewHolder, position: Int) {
+        holder.itemTitle.text = myTitles[position]
+        holder.itemDetail.text = myDetails[position]
+//        holder.itemInstructions.text = myInstructions[position]
+        holder.itemImage.setImageResource(myImages[position])
+        holder.itemSuggestions.text = suggestions[position]
+    }
+}
+
+class BrowseViewHolder(val view: View): ViewHolder(view) {
+    var itemImage: ImageView
+    var itemTitle: TextView
+    var itemDetail: TextView
+    var itemSuggestions: TextView
+    var findMore: TextView
+    lateinit var itemName: String
+
+
+    init {
+        button = itemView.findViewById(R.id.button)
+
+        itemImage = itemView.findViewById(R.id.item_image)
+        itemTitle = itemView.findViewById(R.id.item_title)
+        itemDetail = itemView.findViewById(R.id.item_detail)
+        itemSuggestions = itemView.findViewById(R.id.suggestion)
+        findMore  = itemView.findViewById(R.id.find_more)
+
+
+        delBtn = itemView.findViewById(R.id.deleteButton)!!
+        addBtn = itemView.findViewById(R.id.addButton)!!
+
+//        button.visibility = INVISIBLE
+
+        // sets position on click
+        view.setOnClickListener {
+            RecyclerAdapter().setPosition(adapterPosition)
         }
+
+        //deletes item when x clicked
+        delBtn!!.setOnClickListener {
+            MainActivity().deleteItem(adapterPosition)
+        }
+
+        //add item when + clicked
+        addBtn!!.setOnClickListener {
+            MainActivity().addToCart(adapterPosition)
+        }
+
+        view.setOnClickListener {
+            itemName = (myTitles[adapterPosition])
+            println(itemName)
+            browseAdapter!!.notifyDataSetChanged()
+        }
+
+    }
+}
+
+class CartRecyclerAdapter : RecyclerView.Adapter<CartViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+
+        val holder = CartViewHolder(itemView.inflate(R.layout.list_layout, parent, false))
+
+        return holder
+    }
+
+    override fun getItemCount(): Int {
+        return myTitles.size
+
+    }
+    fun setPosition(newPosition:Int) {
+        position = newPosition
+    }
+
+    fun getPosition(): Int {
+        return position
+    }
+
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        holder.itemTitle.text = myTitles[position]
+        holder.itemDetail.text = myDetails[position]
+//        holder.itemInstructions.text = myInstructions[position]
+        holder.itemImage.setImageResource(myImages[position])
+        holder.itemSuggestions.text = suggestions[position]
     }
 }
 
@@ -111,34 +237,32 @@ class CartViewHolder(val view: View): ViewHolder(view) {
     var itemImage: ImageView
     var itemTitle: TextView
     var itemDetail: TextView
+    var itemSuggestions: TextView
 
 
     init {
         itemImage = itemView.findViewById(R.id.item_image)
         itemTitle = itemView.findViewById(R.id.item_title)
         itemDetail = itemView.findViewById(R.id.item_detail)
+        itemSuggestions = itemView.findViewById(R.id.suggestion)
+
 
         delBtn = itemView.findViewById(R.id.deleteButton)!!
+        addBtn = itemView.findViewById(R.id.addButton)!!
+
+        // sets position on click
+        view.setOnClickListener {
+            RecyclerAdapter().setPosition(adapterPosition)
+        }
 
         //deletes item when x clicked
         delBtn!!.setOnClickListener {
             MainActivity().deleteItem(adapterPosition)
         }
 
-        view.setOnClickListener {
-            RecyclerAdapter().setPosition(adapterPosition)
-
-            //Changes color on click
-//            if (clicked) {
-//                view.setBackgroundColor(BLACK)
-//            }
-//            else
-//            {
-//                view.setBackgroundColor(BLACK)
-//            }
-//            MainActivity().deleteItem(adapterPosition)
-            adapter?.notifyDataSetChanged()
-
+        //add item when + clicked
+        addBtn!!.setOnClickListener {
+            MainActivity().addToCart(adapterPosition)
         }
     }
 }
